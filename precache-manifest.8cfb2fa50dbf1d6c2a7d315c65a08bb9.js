967808d6182e5e60aee1082e121effb5f9d329cb
sz4m <saleh0sm2@gmail.com>
2020-07-17 15:19:02 +0300

    Updates

---
+++ precache-manifest.8cfb2fa50dbf1d6c2a7d315c65a08bb9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbcbed1aa923a96a9b0ac7efc785a6a3",
+    "url": "/OPTL/index.html"
+  },
+  {
+    "revision": "882f5c27b3336f0da86a",
+    "url": "/OPTL/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d1328010d5d55bdca67e",
+    "url": "/OPTL/static/css/main.6fe9ed0d.chunk.css"
+  },
+  {
+    "revision": "882f5c27b3336f0da86a",
+    "url": "/OPTL/static/js/2.5ff4287f.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/OPTL/static/js/2.5ff4287f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1328010d5d55bdca67e",
+    "url": "/OPTL/static/js/main.86ebb7e0.chunk.js"
+  },
+  {
+    "revision": "7b43eecac5f38eec7455",
+    "url": "/OPTL/static/js/runtime-main.1a085b3d.js"
+  },
+  {
+    "revision": "34b4107f8da0741728db94d961a3d569",
+    "url": "/OPTL/static/media/photo_2019-05-01_17-44-31.34b4107f.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/OPTL/precache-manifest.8cfb2fa50dbf1d6c2a7d315c65a08bb9.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/OPTL/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
